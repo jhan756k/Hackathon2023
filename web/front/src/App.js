d46f5e03452acbc0aeaff56fad1333b2f988680b
Jooney <jhan756k@gmail.com>
2023-08-04 07:12:21 +0900

    final

--- web/front/src/App.js
+++ web/front/src/App.js
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Mainpage from "./Page/Mainpage";
+import Aipage from "./Page/Aipage";
+import Homepage from "./Page/Homepage";
+import Hospitalpage from "./Page/Hospitalpage";
+import Consultpage from "./Page/Consultpage";
+import Communitypage from "./Page/Communitypage";
+import Safetypage from "./Page/Safetypage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Mainpage />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/ai" element={<Aipage />} />
+        <Route path="/hospital" element={<Hospitalpage />} />
+        <Route path="/consult" element={<Consultpage />} />
+        <Route path="/community" element={<Communitypage />} />
+        <Route path="/safety" element={<Safetypage />} />
       </Routes>
     </BrowserRouter>
   );
--- web/front/src/Component/Navbar.jsx
+++ web/front/src/Component/Navbar.jsx
@@ -6,27 +6,32 @@ const Navbar = () => {
     <div>
       <div className="navbar">
         <div className="titlename">
-          <div className="navtitle">Lorem Ipsum</div>
-          <a href="#" className="navlogin">
+          <a href="/" className="navtitle">
+            <img src="images/logo.png" alt="" className="navimg" />
+          </a>
+          <a href="/" className="navlogin">
             로그인/회원가입
           </a>
         </div>
         <div className="navmmenu">
           <div className="navmenuitems">
-            <a href="#" className="navmenuitemtitle">
+            <a href="/" className="navmenuitemtitle">
               홈
             </a>
-            <a href="#" className="navmenuitem">
+            <a href="/ai" className="navmenuitem">
               AI 어시스턴트
             </a>
-            <a href="#" className="navmenuitem">
+            <a href="/consult" className="navmenuitem">
               노무사 상담
             </a>
-            <a href="#" className="navmenuitem">
+            <a href="/hospital" className="navmenuitem">
               병원 찾기
             </a>
-            <a href="#" className="navmenuitem">
+            <a href="/community" className="navmenuitem">
               커뮤니티
+            </a>
+            <a href="/safety" className="navmenuitem">
+              안전평가
             </a>
           </div>
         </div>
